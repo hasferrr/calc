981ff9568f5a12b87e774f000c01242cdffb384c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-06-09 06:04:49 +0700

    add color

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 // @ts-check
 const container = document.createElement('div');
-document.body.appendChild(container);
-console.log(document)
+document.querySelector('#main')?.appendChild(container);
 
-const WIDTH = 280;
-const HEIGHT = 560;
+const WIDTH = 280 * 1.1;
+const HEIGHT = 560 * 1.1;
 
 let operator;
 let operandLeft;
 let operandRight;
 
-container.id = 'container';
+container.className = 'container';
 container.style.width = WIDTH + 'px';
 container.style.height = HEIGHT + 'px';
 
 displayCalculatorDisplay();
 displayButton();
+roundButton();
 
 function displayCalculatorDisplay() {
     const display = document.createElement('div');
-    display.classList.add('display');
+    display.className = 'display';
     display.style.width = WIDTH + 'px';
     display.style.height = HEIGHT * (1 - 0.625) + 'px';
 
@@ -28,14 +28,14 @@ function displayCalculatorDisplay() {
 
 function displayButton() {
     const row = document.createElement('div');
-    row.classList.add('row');
+    row.className = 'row';
     row.style.width = WIDTH + 'px';
     row.style.height = HEIGHT * 0.625 / 5 + 'px';
 
     const button = document.createElement('div')
-    button.classList.add('button');
-    button.style.width = WIDTH + 'px';
-    button.style.height = HEIGHT * 0.625 / 5 + 'px';
+    button.className = 'button';
+    button.style.width = WIDTH / 4 - 2 + 'px';
+    button.style.height = HEIGHT * 0.625 / 5 - 2 + 'px';
 
     for (let i = 0; i < 4; i++) {
         row.appendChild(button.cloneNode(true));
@@ -44,6 +44,14 @@ function displayButton() {
     for (let i = 0; i < 5; i++) {
         container.appendChild(row.cloneNode(true));
     }
+}
+
+function roundButton() {
+    const lastRow = container.lastElementChild;
+    // @ts-ignore
+    lastRow.firstElementChild.style.borderBottomLeftRadius = '30px';
+    // @ts-ignore
+    lastRow.lastElementChild.style.borderBottomRightRadius = '30px';
 }
 
 function operate(operator, a, b) {
