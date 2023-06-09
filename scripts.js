eeb1695d615a7527bd45caf88888f3d5ce2f5b43
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-06-09 15:14:28 +0700

    refactor into classes

--- scripts.js
+++ scripts.js
@@ -1,137 +1,135 @@
 // @ts-check
-const container = document.createElement('div');
-const SCALE = 1.15;
-const WIDTH = 290 * SCALE;
-const HEIGHT = 560 * SCALE;
-
-let operator;
-let operandLeft;
-let operandRight;
-
-document.querySelector('#main')?.appendChild(container);
-container.classList.add('container');
-container.style.width = WIDTH + 'px';
-container.style.height = HEIGHT + 'px';
-
-displayCalculatorScreen();
-displayButton();
-roundButton();
-displayButtonText();
-displayCalculationText();
-addClassButton();
-
-enableClickToAssignNumber();
-enableClickToAssignOperator();
-enableEvaluate();
-enableDeleteButton();
-enableClearButton();
-
-
-function displayCalculatorScreen() {
-    const display = document.createElement('div');
-    display.classList.add('display');
-    display.style.width = WIDTH + 'px';
-    display.style.height = HEIGHT * (1 - 0.625) + 'px';
-
-    container.appendChild(display.cloneNode(true));
-}
-
-function displayButton() {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.style.width = WIDTH + 'px';
-    row.style.height = HEIGHT * 0.625 / 5 + 'px';
-
-    const button = document.createElement('div')
-    button.classList.add('button');
-    button.style.width = WIDTH / 4 + 'px';
-    button.style.height = HEIGHT * 0.625 / 5 + 'px';
-
-    for (let i = 0; i < 4; i++) {
-        row.appendChild(button.cloneNode(true));
-    }
-
-    for (let i = 0; i < 5; i++) {
-        container.appendChild(row.cloneNode(true));
+class Calculator {
+    /**
+     * @param {Element} container
+     */
+    constructor(container) {
+        this.container = container;
     }
 }
 
-function roundButton() {
-    const lastRow = container.lastElementChild;
-    // @ts-ignore
-    lastRow.firstElementChild.style.borderBottomLeftRadius = '30px';
-    // @ts-ignore
-    lastRow.lastElementChild.style.borderBottomRightRadius = '30px';
-}
+class DisplayCalculator extends Calculator {
+    static SCALE = 1.15;
+    static WIDTH = 290 * DisplayCalculator.SCALE;
+    static HEIGHT = 560 * DisplayCalculator.SCALE;
 
-function displayButtonText() {
-    const symbol = [
-        'AC', '/', 'x', 'DEL',
-        '7', '8', '9', '%',
-        '4', '5', '6', '-',
-        '1', '2', '3', '+',
-        '', '0', '.', '='];
-    const buttons = container.querySelectorAll('.button');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].textContent = symbol[i];
+    /**
+     * @param {HTMLDivElement} container
+     */
+    constructor(container) {
+        super(container);
+        container.style.width = DisplayCalculator.WIDTH + 'px';
+        container.style.height = DisplayCalculator.HEIGHT + 'px';
     }
-}
 
-function displayCalculationText() {
-    const display = document.querySelector('.display');
-    const calculations = document.createElement('div');
-    const result = document.createElement('div');
-    const box = document.createElement('div');
+    displayCalculatorScreen() {
+        const display = document.createElement('div');
+        display.classList.add('display');
+        display.style.width = DisplayCalculator.WIDTH + 'px';
+        display.style.height = DisplayCalculator.HEIGHT * (1 - 0.625) + 'px';
 
-    //@ts-ignore
-    const DISPLAY_HEIGHT = Number(display.style.height.slice(0, -2)); //remove 'px'
+        this.container.appendChild(display.cloneNode(true));
+    }
 
-    calculations.classList.add('calculations');
-    result.classList.add('result');
+    displayButton() {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.width = DisplayCalculator.WIDTH + 'px';
+        row.style.height = DisplayCalculator.HEIGHT * 0.625 / 5 + 'px';
 
-    let height = 80;
-    let width = WIDTH / SCALE - 35;
+        const button = document.createElement('div')
+        button.classList.add('button');
+        button.style.width = DisplayCalculator.WIDTH / 4 + 'px';
+        button.style.height = DisplayCalculator.HEIGHT * 0.625 / 5 + 'px';
 
-    box.style.height = `${20 * SCALE}px`;
-    calculations.style.height = `${DISPLAY_HEIGHT - height * SCALE}px`;
-    result.style.height = `${height * SCALE}px`;
-
-    calculations.style.width = `${width * SCALE}px`;
-    result.style.width = `${width * SCALE}px`;
-
-    calculations.style.fontSize = `${18 * SCALE}px`;
-    result.style.fontSize = `${42 * SCALE}px`;
-
-    calculations.textContent = '13 x 15';
-    result.textContent = '650';
-
-    // Append to .display
-    display?.appendChild(box);
-    display?.appendChild(calculations);
-    display?.appendChild(result);
-    display?.appendChild(box.cloneNode(true));
-}
-
-
-function addClassButton() {
-    const classWishList = [
-        ['btn-ac', 'btn-div', 'btn-mult', 'btn-del'],
-        ['btn-7', 'btn-8', 'btn-9', 'btn-percent'],
-        ['btn-4', 'btn-5', 'btn-6', 'btn-sub'],
-        ['btn-1', 'btn-2', 'btn-3', 'btn-add'],
-        ['', 'btn-0', 'btn-dot', 'btn-equal']
-    ];
-
-    const rows = container.querySelectorAll('.row');
-
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let buttons = row.querySelectorAll('.button');
-
-        for (let j = 0; j < buttons.length; j++) {
-            let button = buttons[j];
-            button.classList.add(classWishList[i][j]);
+        for (let i = 0; i < 4; i++) {
+            row.appendChild(button.cloneNode(true));
         }
+
+        for (let i = 0; i < 5; i++) {
+            this.container.appendChild(row.cloneNode(true));
+        }
+
+        this.#addClassButton();
+        this.#roundButton();
+    }
+
+    #addClassButton() {
+        const classWishList = [
+            ['btn-ac', 'btn-div', 'btn-mult', 'btn-del'],
+            ['btn-7', 'btn-8', 'btn-9', 'btn-percent'],
+            ['btn-4', 'btn-5', 'btn-6', 'btn-sub'],
+            ['btn-1', 'btn-2', 'btn-3', 'btn-add'],
+            ['btn-empty', 'btn-0', 'btn-dot', 'btn-equal']
+        ];
+
+        const rows = this.container.querySelectorAll('.row');
+
+        for (let i = 0; i < rows.length; i++) {
+            let row = rows[i];
+            let buttons = row.querySelectorAll('.button');
+
+            for (let j = 0; j < buttons.length; j++) {
+                let button = buttons[j];
+                button.classList.add(classWishList[i][j]);
+            }
+        }
+    }
+
+    #roundButton() {
+        const lastRow = this.container.lastElementChild;
+        // @ts-ignore
+        lastRow.firstElementChild.style.borderBottomLeftRadius = '30px';
+        // @ts-ignore
+        lastRow.lastElementChild.style.borderBottomRightRadius = '30px';
+    }
+
+    displayButtonText() {
+        const symbol = [
+            'AC', '/', 'x', 'DEL',
+            '7', '8', '9', '%',
+            '4', '5', '6', '-',
+            '1', '2', '3', '+',
+            '', '0', '.', '='];
+        const buttons = this.container.querySelectorAll('.button');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].textContent = symbol[i];
+        }
+    }
+
+    displayCalculationText() {
+        const display = document.querySelector('.display');
+        const calculations = document.createElement('div');
+        const result = document.createElement('div');
+        const box = document.createElement('div');
+
+        //@ts-ignore
+        const DISPLAY_HEIGHT = Number(display.style.height.slice(0, -2)); //remove 'px'
+
+        calculations.classList.add('calculations');
+        result.classList.add('result');
+
+        let height = 80;
+        let width = DisplayCalculator.WIDTH / DisplayCalculator.SCALE - 35;
+
+        box.style.height = 20 * DisplayCalculator.SCALE + 'px';
+        calculations.style.height = DISPLAY_HEIGHT - height * DisplayCalculator.SCALE + 'px';
+        result.style.height = height * DisplayCalculator.SCALE + 'px';
+
+        calculations.style.width = width * DisplayCalculator.SCALE + 'px';
+        result.style.width = width * DisplayCalculator.SCALE + 'px';
+
+        calculations.style.fontSize = 18 * DisplayCalculator.SCALE + 'px';
+        result.style.fontSize = 42 * DisplayCalculator.SCALE + 'px';
+
+        calculations.textContent = '13 x 15';
+        result.textContent = '650';
+
+        // Append to .display
+        display?.appendChild(box);
+        display?.appendChild(calculations);
+        display?.appendChild(result);
+        display?.appendChild(box.cloneNode(true));
     }
 }
 
@@ -166,3 +164,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+
+//@ts-ignore
+const calc = new DisplayCalculator(document.querySelector('.container'));
+
+calc.displayCalculatorScreen();
+calc.displayButton();
+calc.displayButtonText();
+calc.displayCalculationText();
+
+enableClickToAssignNumber();
+enableClickToAssignOperator();
+enableEvaluate();
+enableDeleteButton();
+enableClearButton();
